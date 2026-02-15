50cf82163f698c489c3e2044657c272b19737db0
kimjeffsj <165725029+kimjeffsj@users.noreply.github.com>
2026-02-14 16:55:27 -0800

    fix: correct meta tag for mobile web app capability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/icon?family=Material+Icons+Round"
           rel="stylesheet"
         />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       </head>
