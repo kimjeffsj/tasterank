b516d68337fe744bf2acabb88ea0cbcadfa1e9e5
kimjeffsj <165725029+kimjeffsj@users.noreply.github.com>
2026-02-14 16:33:33 -0800

    feat: add icons and update manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,17 +12,17 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#ec7f13",
     icons: [
       {
-        src: "/icons/icon-192.png",
+        src: "/icons/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icons/icon-512.png",
+        src: "/icons/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
       {
-        src: "/icons/icon-512-maskable.png",
+        src: "/icons/icon-maskable-512x512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
